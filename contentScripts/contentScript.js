9473234bd118f508f89acbb80856a71692ce1a21
wangqz <2809755637@qq.com>
2022-12-30 10:14:09 +0800

    优化 跳转链接判断, 已知问题 页面 body innerText.length<100

--- contentScripts/contentScript.js
+++ contentScripts/contentScript.js
@@ -6,59 +6,12 @@ function httpString(s) {
 	return s;
 }
 
-// 跳转倒计时
-const timeOut = function (button, target) {
-	// location.href = decodeURIComponent(target);
-	button.innerText = "正在跳转...";
-	window.close();
-	window.open(decodeURIComponent(target));
-};
-
-// 获取页面dom
-function q(options = {}) {
-	options.c = options.c ? options.c : "getElementsByClassName";
-	return document[options.c](options.el)[0];
-}
-// 网址库
-const urlLib = {
-	"link.juejin.cn/": {
-		el: "btn",
-	},
-	"link.csdn.net/": {
-		el: "loading-btn",
-	},
-	"link.zhihu.com/": {
-		el: "button",
-	},
-	"developers.weixin.qq.com/community/middlepage/href": {
-		el: "text_area_title",
-	},
-	"gitee.com/link": {
-		el: "button",
-	},
-	"www.oschina.net/action/GoToLink": {
-		el: "link-button",
-	},
-};
-
-const LINKLIB = {
-	csdn: "link.csdn.net/",
-};
-
 window.onload = function () {
-	const currHost = location.host + location.pathname;
-	const jump = urlLib[currHost];
-	if (jump) {
-		const text = document.getElementsByTagName("body")[0].innerText;
-		const url = httpString(text)[0];
-		url && timeOut(q(jump), url);
-	}
-
 	// angular 自动重定向到中文文档
 	if (location.host == "angular.io") {
 		location.href = location.href.replaceAll(".io", ".cn");
 	}
-	
+
 	// antvue 自动重定向到中文文档
 	if (location.host == "antdv.com") {
 		if (!location.href.includes("overview-cn")) {
@@ -66,12 +19,14 @@ window.onload = function () {
 		}
 	}
 
-	// 隐藏百度广告
-	if (location.host == "baike.baidu.com") {
-		var ad = document.getElementById("side_box_unionAd");
-		ad.style.display = "none";
+	// 自动跳转
+	const bodyText = document.body.innerText;
+	const urls = httpString(bodyText);
+	if (bodyText.length < 100 && urls && urls.length == 1 && bodyText.includes("安全")) {
+		window.close();
+		window.open(urls[0]);
 	}
-	// 移除页面灵车灰
-	document.getElementsByTagName("html")[0].style.filter = "unset"
 
+	// 移除页面灵车灰
+	document.getElementsByTagName("html")[0].style.filter = "unset";
 };
